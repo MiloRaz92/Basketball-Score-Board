6b79881582383f317d11bc8d1d5e69aed96a7f2a
MiloRaz92 <112229577+MiloRaz92@users.noreply.github.com>
2023-01-03 19:18:48 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let homeScoreEl = document.getElementById("homescore-el")
+let guestScoreEl = document.getElementById("guestscore-el")
+let homeScore = guestScore = 0
+
+function home1() {
+    homeScoreEl.textContent = Number(homeScoreEl.textContent) + 1
+}
+
+function home2() {
+    homeScoreEl.textContent = Number(homeScoreEl.textContent) + 2
+}
+
+function home3() {
+    homeScoreEl.textContent = Number(homeScoreEl.textContent) + 3
+}
+
+
+function guest1() {
+    guestScoreEl.textContent = Number(guestScoreEl.textContent) + 1
+}
+
+function guest2() {
+    guestScoreEl.textContent = Number(guestScoreEl.textContent) + 2
+}
+
+function guest3() {
+    guestScoreEl.textContent = Number(guestScoreEl.textContent) + 3
+}
+
+function newgame() {
+    homeScoreEl.textContent = 0
+    guestScoreEl.textContent = 0
+}
